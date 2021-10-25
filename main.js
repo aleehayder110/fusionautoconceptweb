6ec8f40817773f92c2d3d0526dddddd9970e4b01
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-10-25 04:38:44 -0700

    third commit

--- main.js
+++ main.js
@@ -67,5 +67,25 @@ var swiper = new Swiper(".mySwiper", {
     autoplay:{
         delay:3000,
         disableOnInteraction:false,
-    }
+    },
+    breakpoints: {  
+   
+        // when window width is <= 320px     
+        320: {       
+           slidesPerView: 1,
+           spaceBetween: 10     
+        },     
+        // when window width is <= 480px     
+        // 480: {       
+        //    slidesPerView: 2,       
+        //    spaceBetween: 20     
+        // },   
+    
+        // when window width is <= 640px     
+        640: {       
+           slidesPerView: 3,       
+           spaceBetween: 30     
+        } 
+    
+     } 
   });
