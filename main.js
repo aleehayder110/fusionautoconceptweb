c37880cc9bc05649358eeba7eb09949d861c41e5
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-10-29 12:11:02 -0700

    add news ticker 29 oct

--- main.js
+++ main.js
@@ -165,3 +165,32 @@ var mySwiper = new Swiper(sliderSelector, options);
 
 // Initialize slider
 mySwiper.init();
+
+//new ticker
+$(function () {
+  var $ticker = $('#news-ticker'),
+    $first = $('li:first-child', $ticker);
+  
+  // put an empty space between each letter so we can 
+  // use break word
+  $('a', $ticker).each(function () {
+      var $this = $(this),
+        text = $this.text();
+     $this.html(text.split('').join('&#8203;'));
+  });
+  
+  // begin the animation
+  function tick($el) {
+      $el.addClass('tick')
+        .on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function () {
+            
+          $el.removeClass('tick');
+            var $next = $el.next('li');
+            $next = $next.length > 0 ? $next : $first;
+          tick($next);
+      });
+  }
+      
+  tick($first);
+  
+});
