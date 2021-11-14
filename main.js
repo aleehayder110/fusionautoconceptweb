bec50e724ee6352305cf48d09c383596e1e0dc27
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-11-15 01:44:01 +0500

    update 15 nov

--- main.js
+++ main.js
@@ -172,13 +172,13 @@ var downArrow = document.getElementById("downArrow");
 let x = 0;
  upArrow.onclick = function(){
    if(x > "-600"){
-    x = x - 300;
+    x = x - 350;
     slide.style.top = x + "px";
    }
  }
  downArrow.onclick = function(){
   if(x < 0){
-   x = x + 300;
+   x = x + 350;
    slide.style.top = x + "px";
   }
 }
