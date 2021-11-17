e4e6818db46b97ee443a71c7e20f5d95f15ef05b
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-11-18 03:48:35 +0500

    update 18 nov

--- main.js
+++ main.js
@@ -292,3 +292,130 @@ window.onload = function() {
 			
 		})
 }
+
+//Big Slider
+
+$(document).ready(function(){
+
+    $("#slideshow > div:gt(0)").hide();
+    
+    var buttons = "<button class=\"slidebtn prev\"><i class=\"fa fa-chevron-circle-left\"></i></button><button class=\"slidebtn next\"><i class=\"fa fa-chevron-circle-right\"></i></button\>";
+    
+    var slidesl = $('.slideitem').length
+    var d = "<li class=\"dot active-dot\">&bull;</li>";
+    for (var i = 1; i < slidesl; i++) {
+      d = d+"<li class=\"dot\">&bull;</li>";
+    }	
+    var dots = "<ul class=\"slider-dots\">" + d + "</ul\>";
+    
+    $("#slideshow").append(dots).append(buttons);
+    var interval = setInterval(slide, 3000);
+    
+    function intslide(func) {
+        if (func == 'start') { 
+         interval = setInterval(slide, 3000);
+        } else {
+            clearInterval(interval);		
+            }
+    }
+    
+    function slide() {
+            sact('next', 0, 1200);
+    }
+        
+    function sact(a, ix, it) {
+            var currentSlide = $('.current');
+            var nextSlide = currentSlide.next('.slideitem');
+            var prevSlide = currentSlide.prev('.slideitem');
+                var reqSlide = $('.slideitem').eq(ix);
+    
+                var currentDot = $('.active-dot');
+              var nextDot = currentDot.next();
+              var prevDot = currentDot.prev();
+                var reqDot = $('.dot').eq(ix);
+            
+            if (nextSlide.length == 0) {
+                  nextDot = $('.dot').first();
+                nextSlide = $('.slideitem').first();
+                }
+    
+            if (prevSlide.length == 0) {
+                  prevDot = $('.dot').last();
+                prevSlide = $('.slideitem').last();
+                }
+                
+            if (a == 'next') {
+                var Slide = nextSlide;
+                var Dot = nextDot;
+                }
+                else if (a == 'prev') {
+                    var Slide = prevSlide;
+                    var Dot = prevDot;
+                    }
+                    else {
+                        var Slide = reqSlide;
+                        var Dot = reqDot;
+                        }
+    
+            currentSlide.fadeOut(it).removeClass('current');
+            Slide.fadeIn(it).addClass('current');
+            
+            currentDot.removeClass('active-dot');
+            Dot.addClass('active-dot');
+    }	
+    
+    $('.next').on('click', function(){
+            intslide('stop');						
+            sact('next', 0, 400);
+            intslide('start');						
+        });//next
+    
+    $('.prev').on('click', function(){
+            intslide('stop');						
+            sact('prev', 0, 400);
+            intslide('start');						
+        });//prev
+    
+    $('.dot').on('click', function(){
+            intslide('stop');
+            var index  = $(this).index();
+            sact('dot', index, 400);
+            intslide('start');						
+        });//prev
+    //slideshow
+    });
+
+
+    //Appointment Form
+    const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+        onlyCountries: ["AE"],
+     utilsScript:
+       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+   });
+
+//    var input = document.querySelector("#phone");
+//     window.intlTelInput(input, {
+//       // allowDropdown: false,
+//       // autoHideDialCode: false,
+//       // autoPlaceholder: "off",
+//       // dropdownContainer: document.body,
+//       // excludeCountries: ["us"],
+//       // formatOnDisplay: false,
+//       // geoIpLookup: function(callback) {
+//       //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+//       //     var countryCode = (resp && resp.country) ? resp.country : "";
+//       //     callback(countryCode);
+//       //   });
+//       // },
+//       // hiddenInput: "full_number",
+//       // initialCountry: "auto",
+//       // localizedCountries: { 'de': 'Deutschland' },
+//       // nationalMode: false,
+//       // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+//       // placeholderNumberType: "MOBILE",
+//       // preferredCountries: ['cn', 'jp'],
+//       // separateDialCode: true,
+//       utilsScript: "build/js/utils.js",
+//     });
+
