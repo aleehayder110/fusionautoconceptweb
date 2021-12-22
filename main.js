d19770ddd9e269e8a13a55e00870f022ad0d5f8a
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-12-23 01:57:34 +0500

    update 23 dec

--- main.js
+++ main.js
@@ -432,11 +432,11 @@ $(document).ready(function(){
   });
 
 
- //oppointment form
-//    var e = document.getElementById("inputState");
-//    var val1 = e.options[e.selectedIndex].value;
-//    var txt = e.options[e.selectedIndex].text;
+//  oppointment form
+   var e = document.getElementById("inputState");
+   var val1 = e.options[e.selectedIndex].value;
+   var txt = e.options[e.selectedIndex].text;
 
-//     document.write("<br />Selected option Value: "+ val1);
-//      document.write("<br />Selected option Text: "+ txt);
+    document.write("<br />Selected option Value: "+ val1);
+     document.write("<br />Selected option Text: "+ txt);
  
