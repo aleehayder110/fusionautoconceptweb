c493cb5706708f78b331158d465ac859eb51d34e
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-12-30 02:04:28 +0500

    update 30  dec

--- main.js
+++ main.js
@@ -393,8 +393,8 @@ $(document).ready(function(){
         utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
    });
 //  oppointment Selected Service
-          var e = document.getElementById("selectValue");
-          var sltVal= e.options[e.selectedIndex].text;
-           console.log(strUser);
+        //   var e = document.getElementById("selectValue");
+        //   var sltVal= e.options[e.selectedIndex].text;
+        //    console.log(strUser);
     //date picker
     
