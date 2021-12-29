29476c0a8f8f6680298afeba1f1e9012dc1f8924
Sajjad Haider <45358081+sajjadhaider512@users.noreply.github.com>
2021-12-29 16:18:23 +0500

    update 29  dec

--- main.js
+++ main.js
@@ -392,52 +392,11 @@ $(document).ready(function(){
         onlyCountries: ["AE"],
         utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
    });
-
-   document.getElementById('form').addEventListener('submit', sendForm);
-
-
 //  oppointment form
+
           var e = document.getElementById("selectValue");
           var strUser = e.value; // 2
           var strUser = e.options[e.selectedIndex].text;
- 
+           console.log(strUser);
     //date picker
-    $(document).ready(function() {
-        $('#datePicker')
-            .datepicker({
-                format: 'mm/dd/yyyy'
-            })
-            .on('changeDate', function(e) {
-                // Revalidate the date field
-                $('#eventForm').formValidation('revalidateField', 'date');
-            });
     
-        $('#eventForm').formValidation({
-            framework: 'bootstrap',
-            icon: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            fields: {
-                name: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The name is required'
-                        }
-                    }
-                },
-                date: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The date is required'
-                        },
-                        date: {
-                            format: 'MM/DD/YYYY',
-                            message: 'The date is not a valid'
-                        }
-                    }
-                }
-            }
-        });
-    });
